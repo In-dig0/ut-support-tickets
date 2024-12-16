7cb24728cdc1b78e69c964e7821192ebb018fc22
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2024-12-16 17:13:23 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,123 +136,6 @@ def check_request_fields(record: dict) -> bool:
     res = all(record.values())
     return res
 
-
-# if "my_text" not in st.session_state:
-#     st.session_state.my_text = ""
-
-# def submit():
-#     st.session_state.my_text = st.session_state.widget
-#     st.session_state.widget = ""
-
-# st.text_input("Enter text here", key="widget", on_change=submit)
-
-# my_text = st.session_state.my_text
-
-# st.write(my_text)
-
-
-# def display_ticket_section() -> pd.DataFrame:
-#     """ """
-
-
-
-#     if submitted:
-#     #     # Make a dataframe for the new ticket and append it to the dataframe in session
-#     #     # state.
-#         last_ticket_number = 10
-#         today = datetime.datetime.now().strftime("%m-%d-%Y")
-#         df_new = pd.DataFrame(
-#              [
-#                  {
-#                      "ID": f"TICKET-{last_ticket_number+1}",
-#                      "Title": request_title,
-#                      "Status": "Open",
-#                      "Priority": priority,
-#                      "Date Submitted": today,
-#                  }
-#              ]
-#          )
-
-#     #     # Show a little success message.
-#         st.write("Ticket submitted! Here are the ticket details:")
-#         st.dataframe(df_new, use_container_width=True, hide_index=True)
-#        st.session_state.df = pd.concat([df_new, st.session_state.df], axis=0)
-
-    # # Show section to view and edit existing tickets in a table.
-    # st.header("Existing tickets")
-    # st.write(f"Number of tickets: `{len(st.session_state.df)}`")
-
-    # st.info(
-    #     "You can edit the tickets by double clicking on a cell. Note how the plots below "
-    #     "update automatically! You can also sort the table by clicking on the column headers.",
-    #     icon="✍️",
-    # )
-
-    # # Show the tickets dataframe with `st.data_editor`. This lets the user edit the table
-    # # cells. The edited data is returned as a new dataframe.
-    # edited_df = st.data_editor(
-    #     st.session_state.df,
-    #     use_container_width=True,
-    #     hide_index=True,
-    #     column_config={
-    #         "Status": st.column_config.SelectboxColumn(
-    #             "Status",
-    #             help="Ticket status",
-    #             options=["Open", "In Progress", "Closed"],
-    #             required=True,
-    #         ),
-    #         "Priority": st.column_config.SelectboxColumn(
-    #             "Priority",
-    #             help="Priority",
-    #             options=["High", "Medium", "Low"],
-    #             required=True,
-    #         ),
-    #     },
-    #     # Disable editing the ID and Date Submitted columns.
-    #     disabled=["ID", "Date Submitted"],
-    # )
-
-# # Show some metrics and charts about the ticket.
-# st.header("Statistics")
-
-# # Show metrics side by side using `st.columns` and `st.metric`.
-# col1, col2, col3 = st.columns(3)
-# num_open_tickets = len(
-#     st.session_state.df[st.session_state.df.Status == "Open"])
-# col1.metric(label="Number of open tickets", value=num_open_tickets, delta=10)
-# col2.metric(label="First response time (hours)", value=5.2, delta=-1.5)
-# col3.metric(label="Average resolution time (hours)", value=16, delta=2)
-
-# # Show two Altair charts using `st.altair_chart`.
-# st.write("")
-# st.write("##### Ticket status per month")
-# status_plot = (
-#     alt.Chart(edited_df)
-#     .mark_bar()
-#     .encode(
-#         x="month(Date Submitted):O",
-#         y="count():Q",
-#         xOffset="Status:N",
-#         color="Status:N",
-#     )
-#     .configure_legend(
-#         orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
-#     )
-# )
-# st.altair_chart(status_plot, use_container_width=True, theme="streamlit")
-
-# st.write("##### Current ticket priorities")
-# priority_plot = (
-#     alt.Chart(edited_df)
-#     .mark_arc()
-#     .encode(theta="count():Q", color="Priority:N")
-#     .properties(height=300)
-#     .configure_legend(
-#         orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
-#     )
-# )
-# st.altair_chart(priority_plot, use_container_width=True, theme="streamlit")
-
 def click_submit_button():
     st.session_state.submit_clicked = True
 
@@ -384,6 +267,88 @@ def save_request_to_sqlitecloud(row:dict, atch: dict) -> None:
         conn.close()
 
     return req_nr, rc    
+
+##################################
+# import sqlite3
+
+# def convertToBinaryData(filename):
+#     # Convert digital data to binary format
+#     with open(filename, 'rb') as file:
+#         blobData = file.read()
+#     return blobData
+
+# def insertBLOB(empId, name, photo, resumeFile):
+#     try:
+#         sqliteConnection = sqlite3.connect('SQLite_Python.db')
+#         cursor = sqliteConnection.cursor()
+#         print("Connected to SQLite")
+#         sqlite_insert_blob_query = """ INSERT INTO new_employee
+#                                   (id, name, photo, resume) VALUES (?, ?, ?, ?)"""
+
+#         empPhoto = convertToBinaryData(photo)
+#         resume = convertToBinaryData(resumeFile)
+#         # Convert data into tuple format
+#         data_tuple = (empId, name, empPhoto, resume)
+#         cursor.execute(sqlite_insert_blob_query, data_tuple)
+#         sqliteConnection.commit()
+#         print("Image and file inserted successfully as a BLOB into a table")
+#         cursor.close()
+
+#     except sqlite3.Error as error:
+#         print("Failed to insert blob data into sqlite table", error)
+#     finally:
+#         if sqliteConnection:
+#             sqliteConnection.close()
+#             print("the sqlite connection is closed")
+
+# insertBLOB(1, "Smith", "E:\pynative\Python\photos\smith.jpg", "E:\pynative\Python\photos\smith_resume.txt")
+# insertBLOB(2, "David", "E:\pynative\Python\photos\david.jpg", "E:\pynative\Python\photos\david_resume.txt")
+################################
+
+###############################
+# import sqlite3
+
+# def writeTofile(data, filename):
+#     # Convert binary data to proper format and write it on Hard Disk
+#     with open(filename, 'wb') as file:
+#         file.write(data)
+#     print("Stored blob data into: ", filename, "\n")
+
+# def readBlobData(empId):
+#     try:
+#         sqliteConnection = sqlite3.connect('SQLite_Python.db')
+#         cursor = sqliteConnection.cursor()
+#         print("Connected to SQLite")
+
+#         sql_fetch_blob_query = """SELECT * from new_employee where id = ?"""
+#         cursor.execute(sql_fetch_blob_query, (empId,))
+#         record = cursor.fetchall()
+#         for row in record:
+#             print("Id = ", row[0], "Name = ", row[1])
+#             name = row[1]
+#             photo = row[2]
+#             resumeFile = row[3]
+
+#             print("Storing employee image and resume on disk \n")
+#             photoPath = "E:\pynative\Python\photos\db_data\\" + name + ".jpg"
+#             resumePath = "E:\pynative\Python\photos\db_data\\" + name + "_resume.txt"
+#             writeTofile(photo, photoPath)
+#             writeTofile(resumeFile, resumePath)
+
+#         cursor.close()
+
+#     except sqlite3.Error as error:
+#         print("Failed to read blob data from sqlite table", error)
+#     finally:
+#         if sqliteConnection:
+#             sqliteConnection.close()
+#             print("sqlite connection is closed")
+
+# readBlobData(1)
+# readBlobData(2)
+
+
+
 
 def main() -> None:
     if 'submit_clicked' not in st.session_state:
