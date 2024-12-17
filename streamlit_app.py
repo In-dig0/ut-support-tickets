d69ae462f824039b89e0cf142ca7efa04007f07c
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2024-12-17 17:02:12 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,46 @@ import pandas as pd
 import streamlit as st
 import sqlitecloud
 import pytz 
-import io 
+import io
+import hmac 
 
 APPNAME = "TORP" #IPH Technical Office Request POC (Proof Of Concept)
 APPCODE = "TORP"
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
 
 def display_app_title() -> None:
     """ Show app title and description """
@@ -351,6 +387,10 @@ def save_request_to_sqlitecloud(row:dict, atch: dict) -> None:
 
 
 def main() -> None:
+    
+    if not check_password():
+        st.stop()
+    
     if 'submit_clicked' not in st.session_state:
         st.session_state.submit_clicked = False
     display_app_title()
