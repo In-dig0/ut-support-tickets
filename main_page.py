b0f97e97b1e2e38155c8228a083e3b487599dc63
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2024-12-18 14:00:32 +0100

    update

--- main_page.py
+++ main_page.py
@@ -1,9 +1,9 @@
 import streamlit as st
-
+import hmac 
 
 def display_app_title() -> None:
     """ Show app title and description """
-    st.set_page_config(page_title="TORP_APP_Main_menù", page_icon="🎯")
+    st.set_page_config(page_title="TORP_APP_main_page", page_icon="🎯")
     st.title("🎯 :blue[TORP APP: Technical Office Request POC]")
     st.write(
         """
@@ -13,9 +13,37 @@ def display_app_title() -> None:
     st.markdown("Powered with Streamlit :streamlit:")
     st.divider()
 
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
 def main():
-#    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
+    if not check_password():
+        st.stop()
     display_app_title()
 
 if __name__ == "__main__":
